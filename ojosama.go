329c329a7a7aaf5581981d93eac23c35e6cf3054
jiro4989 <jiroron666@gmail.com>
2022-06-19 02:30:17 +0900

    :+1: ドキュメンテーションコメントを追加

--- ojosama.go
+++ ojosama.go
@@ -133,6 +133,7 @@ func convertMulti(tokens []tokenizer.Token, i int, opt *ConvertOption) (string, 
 	return "", -1, false
 }
 
+// matchExcludeRule は除外ルールと一致するものが存在するかを判定する。
 func matchExcludeRule(data tokenizer.TokenData) bool {
 excludeLoop:
 	for _, c := range excludeRules {
@@ -286,6 +287,13 @@ func appendPoliteWord(data tokenizer.TokenData, tokens []tokenizer.Token, i int,
 	return surface
 }
 
+// equalsFeatures は Features が等しいかどうかを判定する。
+//
+// featuresが空の時は * が設定されているため、 * が出現したら以降は無視する。
+//
+// 例えば {名詞,代名詞,一般,*,*} と {名詞,代名詞,一般} を比較したとき、単純にス
+// ライスを完全一致で比較すると false になるが、この関数に関しては * 以降を無視
+// するため true になる。
 func equalsFeatures(a, b []string) bool {
 	var a2 []string
 	for _, v := range a {
